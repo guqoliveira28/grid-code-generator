3ce38f55e0ee7d421a09cde7a2398436e09afacc
Gustavo Oliveira <guqoliveira@gmail.com>
2025-02-06 23:31:01 +0000

    Fix counter overflow method and added comments

--- backend/src/helpers/counterOverflow.ts
+++ backend/src/helpers/counterOverflow.ts
@@ -1,11 +1,16 @@
 
-// Exception: If the count is larger than 9, divide the count
-// by the lowest integer possible in order to get a value
-// lower or equal to 9
+/**
+ * Validate if count is larger than 9, if true find the lowest
+ * possible integer to get a value lower or equal than 9 and returns
+ * the devided value
+ * 
+ * @param count as number
+ * @returns a number lower or equal to 9
+ */
 export function handleCounterOverflow(count: number) {
     if (count > 9) {
         let divisor = 1;
-        while (count / divisor > 9) {
+        while (Math.floor(count / divisor) >= 9) {
             divisor++;
         }
         return Math.floor(count / divisor);
