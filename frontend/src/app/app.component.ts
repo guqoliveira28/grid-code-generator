83bac617c9906b47279f85d9304cde4984e4e001
Gustavo Oliveira <guqoliveira@gmail.com>
2025-02-08 17:46:26 +0000

    Header - Character input, clock placeholder, Generate grid button | Grid

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+const lineTemplate = Array.from({ length: 10 }, (v, k) => '');
+const gridTemplate = Array.from({ length: 10 }, (v, k) => lineTemplate)
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'Generator Page';
+    grid = gridTemplate;
 }
